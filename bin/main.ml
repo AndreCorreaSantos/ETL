bbcf3d782473b8f52ff8b9acef3047d207831434
AndreCorreaSantos <acorreasantos2002@gmail.com>
2025-02-21 16:44:14 -0300

    checkpoints

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,23 @@
-let () = print_endline "Hello, World!"
+open ETL.Impure
+
+let sample_csv =
+  {|"id","name","color",
+    "1","apple","Red",
+    "2","lemon","Yellow",
+    "3","berry","Blue",|}
+
+(* Main processing *)
+let () =
+  parse_fruits sample_csv
+  |> List.iter (function
+    | Ok fruit ->
+        Printf.printf "%d %s %s\n"
+          fruit.id
+          fruit.name
+          (show_color fruit.color)
+    | Error `Invalid_id ->
+        print_endline "Invalid_id"
+    | Error `Unknown_color ->
+        print_endline "Unknown_color"
+    | Error `Missing_name ->
+        print_endline "Missing_name")
---
+++ readme.md
@@ -0,0 +1,12 @@
+### 1 setup project with dune
+
+### 2 create helper functions to read data from csv
+
+### 3 create types to store data
+
+
+
+
+#### Notas:
+
+Fazer group by da tabela Order_item pela coluna order_id para obter os itens de cada order.
