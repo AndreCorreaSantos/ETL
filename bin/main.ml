b84b5d89b519db54a83e8cd0981a9d6928a61fb5
AndreCorreaSantos <acorreasantos2002@gmail.com>
2025-02-21 19:16:05 -0300

    working order and item parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,20 @@
-open Etl.Impure
+open Etl.Order
+open Etl.Item
 
-let sample_csv = 
+let sample_order_csv = 
 {|"id","client_id","order_date","status","origin"
 "1","112","2024-10-02T03:05:39","Pending","P"|};;
+
+let sample_item_csv = 
+{|"order_id","product_id","quantity","price","tax"
+"12","224","8","139.42","0.12"|};;
 
 (* Main processing *)
 (* 
 Printf.printf "teste" *)
 
 let () =
-  parse_orders sample_csv
+  parse_orders sample_order_csv
   |> List.iter (function
     | Ok order ->
         Printf.printf "%d %d %s %s %s\n"
@@ -26,5 +31,19 @@ let () =
         print_endline "Unknown_status"
     | Error `Unknown_origin ->
         print_endline "Unknown_origin"
-    | _ -> print_endline "Unknown_output"  
+      )
+
+let () =
+  parse_items sample_item_csv
+  |> List.iter (function
+    | Ok item ->
+        Printf.printf "%d %d %f %f\n"
+          item.order_id
+          item.quantity
+          item.price
+          item.tax
+    | Error `Invalid_int ->
+        print_endline "Invalid_int"
+    | Error `Invalid_float ->
+        print_endline "Invalid_float"
       )
